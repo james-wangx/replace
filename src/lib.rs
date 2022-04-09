8797c326a2c20da6326cbe5c825c4cb00a10414f
James <pineapple_cpp@163.com>
2022-04-09 15:51:38 +0800

    Add: command line parser

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use std::{error::Error, fs};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    /// The target file or directory
+    target: String,
+
+    /// The string to be replace
+    string: String,
+
+    /// The result string, after replace
+    switch: String,
+}
+
+impl Args {
+    pub fn replace(&self, target: &str) -> Result<(), Box<dyn Error>> {
+        let md = fs::metadata(target)?;
+        
+        if md.is_file() {
+            let contents = fs::read_to_string(target)?;
+            let results = contents.replace(&self.string, &self.switch);
+            fs::write(target, results)?;
+            println!("Replaced in file {}", target);
+            return  Ok(());
+        } else {
+            for entry in fs::read_dir(target)? {
+                let path = entry?.path();
+                self.replace(path.to_str().unwrap())?;
+            }
+            Ok(())
+        }
+    }
+
+    pub fn get_target(&self) -> &str {
+        &self.target
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,14 @@
-use std::{
-    env,
-    error::Error,
-    fs::{self, metadata},
-    process,
-};
+use std::process;
+
+use clap::Parser;
+use replace::Args;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 4 {
-        eprintln!("Not enough arguments.");
-        process::exit(1);
-    }
+    let args = Args::parse();
 
-    let target = &args[1];
-    let string = &args[2];
-    let switch = &args[3];
-
-    if let Err(e) = replace(target, string, switch) {
+    
+    if let Err(e) = args.replace(args.get_target()) {
         eprintln!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn replace(target: &str, string: &str, switch: &str) -> Result<(), Box<dyn Error>> {
-    let md = metadata(target)?;
-
-    if md.is_file() {
-        let contents = fs::read_to_string(target)?;
-        let results = contents.replace(string, switch);
-        fs::write(target, results)?;
-        println!("Replace in file {}, done.", target);
-        return Ok(());
-    } else {
-        for entry in fs::read_dir(target)? {
-            let path = entry?.path();
-            replace(path.to_str().unwrap(), string, switch)?;
-        }
-        Ok(())
     }
 }
