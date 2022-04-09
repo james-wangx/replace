85bf588008e25da0e56f01502b0fde908fa64158
James <pineapple_cpp@163.com>
2022-04-09 11:30:56 +0800

    Update: replace string in one file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{env, error::Error, fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        eprintln!("Not enough arguments.");
+        process::exit(1);
+    }
+
+    let filename = &args[1];
+    let target = &args[2];
+    let switch = &args[3];
+
+    if let Err(e) = replace(filename, target, switch) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+    
+}
+
+fn replace(filename: &str, target: &str, switch: &str) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(filename)?;
+    let results = contents.replace(target, switch);
+    fs::write(filename, results)?;
+
+    Ok(())
 }
