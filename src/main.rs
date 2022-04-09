b63259abd96f95a4ce6cdb4d730b5a48d8b66fe9
James <pineapple_cpp@163.com>
2022-04-09 14:15:04 +0800

    Update: replace string in directory and file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{env, error::Error, fs, process};
+use std::{
+    env,
+    error::Error,
+    fs::{self, metadata},
+    process,
+};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,21 +12,30 @@ fn main() {
         process::exit(1);
     }
 
-    let filename = &args[1];
-    let target = &args[2];
+    let target = &args[1];
+    let string = &args[2];
     let switch = &args[3];
 
-    if let Err(e) = replace(filename, target, switch) {
+    if let Err(e) = replace(target, string, switch) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
-    
 }
 
-fn replace(filename: &str, target: &str, switch: &str) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(filename)?;
-    let results = contents.replace(target, switch);
-    fs::write(filename, results)?;
+fn replace(target: &str, string: &str, switch: &str) -> Result<(), Box<dyn Error>> {
+    let md = metadata(target)?;
 
-    Ok(())
+    if md.is_file() {
+        let contents = fs::read_to_string(target)?;
+        let results = contents.replace(string, switch);
+        fs::write(target, results)?;
+        println!("Replace in file {}, done.", target);
+        return Ok(());
+    } else {
+        for entry in fs::read_dir(target)? {
+            let path = entry?.path();
+            replace(path.to_str().unwrap(), string, switch)?;
+        }
+        Ok(())
+    }
 }
